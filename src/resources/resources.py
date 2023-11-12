5eff89c219293bdbb3172c1a222c91e889bbff7f
Karel Zeman <karel.zeman@post.cz>
2023-11-12 22:22:30 +0100

    Last changes

--- src/resources/resources.py
+++ src/resources/resources.py
@@ -1,10 +1,11 @@
 import pika
 
 def rabbitmq_connect(host: str = 'localhost', queue_name: str = 'default'):
+    """
+    """
     connection = pika.BlockingConnection(pika.ConnectionParameters(host))
     channel = connection.channel()
 
-    # pokus o nove vytvoreni fronty ve skutecnosti neovlivni jiz existujici frontu
     channel.queue_declare(queue=queue_name)
     return connection, channel
 
